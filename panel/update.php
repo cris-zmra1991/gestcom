c431cccd8be7711417afcad7a0dbb6783ca2415a
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-11-15 08:43:07 -0500

    actualizado el codigo para la actualizacion de la informacion de los contratos

--- panel/update.php
+++ panel/update.php
@@ -10,61 +10,18 @@ if (isset($_SESSION['usuario'])) {
     }
     else{
     $name = $_GET["name"];
-    $contrato = $database->select("contratos","*",["num_proforma" => $name]);
-    foreach ($contrato as $dato) {
-      $nprof = $dato['num_proforma'];
-      $proveedor = $dato['proveedor'];
-      $objeto = $dato['objeto'];
-      $vig = $dato['vigencia'];
-      $tvig = $dato['tipo_vigencia'];
-      $valor = $dato['valor'];
-      $tvalor = $dato['moneda'];
+    $contrato = $database->select("contratos","*",["id_cont" => $name]);
+    foreach ($contrato as $datos) {
+      $nprof = $datos['num_proforma'];
+      $proveedor = $datos['proveedor'];
+      $nproveedor = $database->select("proveedores","*",["id_prov" => $proveedor]);
+      $objeto = $datos['objeto'];
+      $vig = $datos['vigencia'];
+      $tvig = $datos['tipo_vigencia'];
+      $valor = $datos['valor'];
+      $tvalor = $datos['moneda'];
     }
     require '../views/update.view.php';
-    }
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      $errores = "";
-
-
-      $actaueb = $_POST['actaueb'];
-      $acuerdoueb = $_POST['acuerdoueb'];
-      $fechaueb = $_POST['fechaueb'];
-      $actaepe = $_POST['actaepe'];
-      $acuerdoepe = $_POST['acuerdoepe'];
-      $fechaepe = $_POST['fechaepe'];
-      $ffirma = $_POST['ffirma'];
-      $numcont = $_POST['numcont'];
-      $status = $_POST['status'];
-      $tipocont = $_POST['tipocont'];
-
-      $count = $database->count("contratos",["num_proforma" => $nprof]);
-      if(empty($count))
-      {
-        $database->insert("contratos",[
-          "num_proforma" => $nprof,
-          "proveedor" => $proveedor,
-          "objeto" => $objeto,
-          "vigencia" => $vig,
-          "tipo_vigencia" => $tvig,
-          "valor" => $valor,
-          "moneda" => $tvalor,
-          "acta_fca" => $actaueb,
-          "acuerdo_fca" => $acuerdoueb,
-          "fecha_fca" => $fechaueb,
-          "acta_epe" => $actaepe,
-          "acuerdo_epe" => $acuerdoepe,
-          "fecha_epe" => $fechaepe,
-          "fecha_firma" => $ffirma,
-          "numero_cont" => $numcont,
-          "estado_cont" => $status,
-          "tipo_cont" => $tipocont
-        ]);
-        $errores = "El contrato se guardó correctamente";
-      }
-      else {
-        $errores = "El contrato ya está registrado (No pueden existir duplicados)";
-      }
     }
 
   }
